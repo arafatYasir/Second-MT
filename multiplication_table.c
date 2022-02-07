21c557e0d21500feece57ec66d307dbedde90959
arafatYasir <arafat.yasir1305@gmail.com>
2022-02-07 17:29:25 +0600

    first commit

---
+++ multiplication_table.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+
+int main()
+{
+    int multiply, multiplier, multiplication;
+
+    printf("-----Multiplication table of 1-----\n");
+    for(multiply = 1; multiply <= 1; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 2-----\n");
+    for(multiply = 2, multiplication = 0; multiply <= 2; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 3-----\n");
+    for(multiply = 3, multiplication = 0; multiply <= 3; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 4-----\n");
+    for(multiply = 4, multiplication = 0; multiply <= 4; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 5-----\n");
+    for(multiply = 5, multiplication = 0; multiply <= 5; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 6-----\n");
+    for(multiply = 6, multiplication = 0; multiply <= 6; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 7-----\n");
+    for(multiply = 7, multiplication = 0; multiply <= 7; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 8-----\n");
+    for(multiply = 8, multiplication = 0; multiply <= 8; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 9-----\n");
+    for(multiply = 9, multiplication = 0; multiply <= 9; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 10-----\n");
+    for(multiply = 10, multiplication = 0; multiply <= 10; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 11-----\n");
+    for(multiply = 11, multiplication = 0; multiply <= 11; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 12-----\n");
+    for(multiply = 12, multiplication = 0; multiply <= 12; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 13-----\n");
+    for(multiply = 13, multiplication = 0; multiply <= 13; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 14-----\n");
+    for(multiply = 14, multiplication = 0; multiply <= 14; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 15-----\n");
+    for(multiply = 15, multiplication = 0; multiply <= 15; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 16-----\n");
+    for(multiply = 16, multiplication = 0; multiply <= 16; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 17-----\n");
+    for(multiply = 17, multiplication = 0; multiply <= 17; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 18-----\n");
+    for(multiply = 18, multiplication = 0; multiply <= 18; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 19-----\n");
+    for(multiply = 19, multiplication = 0; multiply <= 19; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+
+    printf("-----Multiplication table of 20-----\n");
+    for(multiply = 20, multiplication = 0; multiply <= 20; multiply++) {
+        for(multiplier = 1; multiplier <= 10; multiplier++) {
+            multiplication = multiplication + multiply;
+            printf("%d x %d = %d\n", multiply, multiplier, multiplication);
+        }
+    }
+    
+    return 0;
+}
